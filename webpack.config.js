0d768b71b4d97a91410300ee686dedb7652676b3
ivan san <sii200192@gmail.com>
2020-12-01 20:35:25 +0700

    Добавлен mini-css-extract-plugin для сбора css в один файл. Важно, sass файлы по прежнему будут в js, так как для них конфиг webpack не поменялся, он поменялся только для css

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -15,12 +16,15 @@ module.exports = {
       template: path.resolve(__dirname, 'src/index.html'),
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[hash].css',
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
